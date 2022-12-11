38d501b5b82a9804da13848318246202463dc1f8
IMAGINARY9 <104233818+IMAGINARY9@users.noreply.github.com>
2022-12-11 18:10:06 +0100

    Destroyer, Cinemachine

---
+++ Fading stroll/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoCache
+{
+    public static List<GameObject> allDestroy = new();
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _destroyDistance;
+    private Vector3 _playerPos;
+    public int Count;
+
+    public override void OnTick()
+    {
+        Count = allDestroy.Count;
+        if(_player != null) _playerPos = _player.transform.position;
+        for (int i = 0; i < allDestroy.Count; i++)
+        {
+            if (allDestroy[i] == null)
+            {
+                allDestroy.Remove(allDestroy[i]);
+                return;
+            }
+            var dist = Vector2.Distance(allDestroy[i].transform.position, _playerPos);
+            if(dist >= _destroyDistance)
+            {
+                Destroy(allDestroy[i]);
+                allDestroy.Remove(allDestroy[i]);
+            }
+        }
+    }
+}
--- Fading stroll/Assets/Scripts/Spawner.cs
+++ Fading stroll/Assets/Scripts/Spawner.cs
@@ -11,12 +11,21 @@ public class Spawner : MonoCache
     {
         InvokeRepeating(nameof(Spawn), 1, _tickRate);
     }
+    //public override void OnTick()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //        Spawn();
+
+    //}
 
     private void Spawn()
     {
         GameObject obj = _prefabs[Random.Range(0, _prefabs.Length)];
-        Vector3 pos = new Vector2(Random.Range(_size.x, _size.y), Random.Range(_size.x, _size.y));
-        Instantiate(obj, pos + transform.localPosition, Quaternion.identity);
+        Vector3 currentPos = transform.position;
+        Vector2 newPos = new Vector2(Random.Range(_size.x, _size.y) + currentPos.x,
+            Random.Range(_size.x, _size.y) + currentPos.y);
+        var newObj = Instantiate(obj, newPos, Quaternion.identity);
+        Destroyer.allDestroy.Add(newObj);
     }
 
 }
