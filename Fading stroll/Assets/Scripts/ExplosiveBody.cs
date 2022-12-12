7923cef64c84efbbe336fe4ee0a75362b1e0475a
IMAGINARY9 <104233818+IMAGINARY9@users.noreply.github.com>
2022-12-12 20:57:58 +0100

    Dependent particles, light

--- Fading stroll/Assets/Scripts/ExplosiveBody.cs
+++ Fading stroll/Assets/Scripts/ExplosiveBody.cs
@@ -9,9 +9,24 @@ public class ExplosiveBody : InteractiveBody
     [System.Obsolete]
     protected void Explode()
     {
+        Particles();
+        Destroy(gameObject);
+    }
+    [System.Obsolete]
+    private void Particles()
+    {
         var particles = Instantiate(destroyParticles, transform.position, Quaternion.identity);
+        var scale = transform.localScale.z;
+
+        var pShape = particles.shape;
+        pShape.radius = scale;
+
+        var pBurst = new ParticleSystem.Burst(0f, (short)(Mass * 100));
+        particles.emission.SetBurst(0, pBurst);
+
+        particles.startSize = scale * 0.1f;
+
         particles.Play();
         Destroy(particles.gameObject, particles.startLifetime);
-        Destroy(gameObject);
     }
 }
---
+++ Fading stroll/Assets/Scripts/LuminousBody.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LuminousBody : InteractiveBody
+{
+    [SerializeField] private Light2D _light;
+
+    protected override void OnAwake()
+    {
+        _light.pointLightOuterRadius = transform.localScale.z;
+        Instantiate(_light, transform.position, Quaternion.identity, transform);
+    }
+
+}
