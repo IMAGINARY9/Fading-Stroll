bf8f4652f3daf8c591b97d192151da90897c5f5b
IMAGINARY9 <104233818+IMAGINARY9@users.noreply.github.com>
2023-01-15 17:19:17 +0100

    Colouring

---
+++ Fading stroll/Assets/Scripts/ColorfulBody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ColorfulBody : InteractiveBody
+{
+    [SerializeField] private float _colorRange;
+
+    protected override void OnAwake()
+    {
+        var sp = GetComponent<SpriteRenderer>();
+        var color = sp.color;
+
+        color.r += Random.Range(-_colorRange, _colorRange);
+        color.g += Random.Range(-_colorRange, _colorRange);
+        color.b += Random.Range(-_colorRange, _colorRange);
+
+        sp.color = color;
+
+    }
+}
--- Fading stroll/Assets/Scripts/RandomizedBody.cs
+++ Fading stroll/Assets/Scripts/RandomizedBody.cs
@@ -1,20 +1,17 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-namespace Assets.Scripts
+public class RandomizedBody : InteractiveBody
 {
-    public class RandomizedBody : InteractiveBody
+    [SerializeField] private Vector2 _sizeLim;
+    [SerializeField] private Vector2 _densityLim;
+
+    protected override void OnAwake()
     {
-        [SerializeField] private Vector2 _sizeLim;
-        [SerializeField] private Vector2 _densityLim;
+        var col = GetComponent<CircleCollider2D>();
+        col.density = Random.Range(_densityLim.x, _densityLim.y);
 
-        protected override void OnAwake()
-        {
-            var col = GetComponent<CircleCollider2D>();
-            col.density = Random.Range(_densityLim.x, _densityLim.y);
-
-            var size = Random.Range(_sizeLim.x, _sizeLim.y);
-            transform.localScale = new Vector3(size, size, size);
-        }
+        var size = Random.Range(_sizeLim.x, _sizeLim.y);
+        transform.localScale = new Vector3(size, size, size);
     }
 }
